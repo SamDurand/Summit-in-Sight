ec9ed5a41079106172b4dfde453762c0c3f777ab
Sam Durand <samsam8848@gmail.com>
2023-02-04 23:53:32 +0100

    Add view from summits

--- View_from_summits.py
+++ View_from_summits.py
@@ -1,14 +1,14 @@
-from summit_lib import*
+from summit_lib import *
 
-data = pd.read_excel("dev/List_Summits_from_wiki.xlsx")
+data = pd.read_excel("data/List_Summits_from_wiki.xlsx")
 grid_locations = pd.DataFrame()
 grid_locations["lat_grid"], grid_locations["lon_grid"] = data["latitude"], data["longitude"]
 
 location_summit = [45.832542 , 6.864717]
 offset_view = 2
 offset_summit = -50
-res = 0.5
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-grid_locations_isvisible = summit_is_visible_multi_locations(grid_locations, location_summit, offset_view, offset_summit)
+grid_locations_processed = summit_is_visible_multi_locations(grid_locations, location_summit, offset_view, offset_summit)
+grid_locations_processed.to_csv("data.csv")
--- view_possible_multi_locations.py
+++ view_possible_multi_locations.py
@@ -4,12 +4,10 @@ top_left_corner, bottom_right_corner = [47 , 5], [45 , 8]
 location_summit = [45.832542 , 6.864717]
 offset_view = 2
 offset_summit = -100
-res = 0.5 #km
+res = 50 #km
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 grid_locations = generate_locations_grid(top_left_corner, bottom_right_corner, res)
 grid_locations_processed = summit_is_visible_multi_locations(grid_locations, location_summit, offset_view, offset_summit)
 grid_locations_processed.to_csv("data.csv")
-
-#plot_locations_map(grid_locations=grid_locations_processed, color="view_possible")
