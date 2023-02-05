b2a44667d2bb37510ed7c75c6bd2f541dcfa55c5
Sam Durand <samsam8848@gmail.com>
2023-02-05 21:27:04 +0100

    Add comments in summit_lib

--- single_location_online.py
+++ single_location_online.py
@@ -14,4 +14,4 @@ offset_view = 2 # Your elevation from the ground of your location_point (can be 
 offset_summit = -50 # How much of the summit you want to see. If 0, you will test your location with exactly the altitude of the summit to spot. 
 # If -100, you will test if you can see from your location the last 100 meters of the summit.
 
-data, view_possible = summit_is_visible_online(location_point, location_summit, plot=True, offset_view=offset_view, offset_summit=offset_summit)
+view_possible, _ = summit_is_visible_online(location_point, location_summit, plot=True, offset_view=offset_view, offset_summit=offset_summit)
--- summit_lib.py
+++ summit_lib.py
@@ -9,39 +9,57 @@ from tqdm import tqdm
 from geopy.geocoders import Nominatim
 
 
-def trigo_to_complex(modulus, arg_z):
-    """Compute a complex number from its trigonometric notation (from argument and modulus)
+def trigo_to_complex(modulus_z, arg_z):
+
+    """Compute a complex number from its polar/trigonometric notation (= from argument and modulus)
+    
+    -> Convertion from polar coordinates to cartesian coordinates.
 
     Args:
-        modulus (_type_): _description_
-        arg_z (_type_): _description_
+        modulus_z (float): modulus of a complex number Z
+        arg_z (float): argument of a complex number Z
 
     Returns:
-        _type_: _description_
+        Z (complex): Complex number composed of real and imaginary parts
     """
-    real = 0
-    imag = 1
-    c = complex(real,imag) # c = 0 + i
 
-    Z = modulus * ( np.cos(arg_z) + c * np.sin(arg_z) )
+    # Create a pure imaginary number 
+    real_part = 0
+    imag_part = 1
+    c = complex(real_part,imag_part) # c = 0 + i
+
+    # Compute Z
+    Z = modulus_z * ( np.cos(arg_z) + c * np.sin(arg_z) )
 
     return Z
 
 def summit_is_visible_online(location_point, location_summit, plot = False, offset_view = 10, offset_summit = -100, samples=False):
-    """Plot full geodesic profile and return if a summit is visible or not.
-    Method: api.opentopodata.org
-    Dataset: srtm30m
+
+    """Determine the visibility of a summit from a single location. Optionaly plot the full geodesic profile.
+
+    Method: api.opentopodata.org (https://www.opentopodata.org/)
+    Dataset: srtm30m (https://wiki.openstreetmap.org/wiki/SRTM)
+
     Args:
-        location_point (_type_): _description_
-        location_summit (_type_): _description_
-        plot (bool, optional): _description_. Defaults to False.
-        offset_view (int, optional): _description_. Defaults to 10.
-        offset_summit (int, optional): _description_. Defaults to -100.
-        samples (bool, optional): _description_. Defaults to False.
+        location_point (list): coordinates in decimal degrees (latitude, longitude) of a location. Example: [46.206939 , 6.147794]
+
+        location_summit (list): coordinates in decimal degrees (latitude, longitude) of a summit. Example: [45.832542 , 6.864717]
+
+        plot (bool, optional): if True, the full geodesic profile is plotted. Convenient to understand how the function actually works. Defaults to False.
+
+        offset_view (int, optional): Your elevation from the ground at your location_point (can be either positive or 0). Defaults to 10.
+
+        offset_summit (int, optional): How much of the summit you want to see. If 0, you will test your location with exactly the altitude of the summit to spot. 
+        If -100, you will test if you can see from your location the last 100 meters of the summit.. Defaults to -100.
+
+        samples (bool or int, optional): Number of equally-spaced samples to query along the path defined by location_point and location_summit. Defaults to False. 
+        If False, the default number of samples is equal to the geodesic distance between location_point and location_summit.
 
     Returns:
-        _type_: _description_
+        view_possible (bool): Return True if the summit is visible from your location.
+        data (pandas DataFrame): table gathering latitude, longitude, elevation, elevation_from_earth_center, angle_from_origin, Z, real_Z, imag_Z, view_possible for all samples of the path       
     """
+
     earth_radius = EARTH_RADIUS
     offset_view, offset_summit, geodesic_distance, geodesic_earth_perimeter = offset_view/1000,  offset_summit/1000, GeodesicDistance(location_point, location_summit).km, (2*np.pi*earth_radius)
 
@@ -57,7 +75,7 @@ def summit_is_visible_online(location_point, location_summit, plot = False, offs
 
     url = "https://api.opentopodata.org/v1/srtm30m"
     json = {"locations": "".join([str(latitude[i]) + "," + str(longitude[i]) + '|' for i in range(samples)]).removesuffix("|"),
-            "interpolation": "cubic",}
+            "interpolation": "cubic"}
     response = requests.post(url, json)
     data = response.json()
 
@@ -66,7 +84,7 @@ def summit_is_visible_online(location_point, location_summit, plot = False, offs
                                                                 np.array([data['results'][i]["location"]["lng"] for i in range(samples)]), \
                                                                 np.add(np.array([data['results'][i]["elevation"] for i in range(samples)], dtype=int)/1000, earth_radius) # Elevation_from_earth_center in km
                                                                 
-    # Generate complex coordinates of each point (relief). We have arg(Z) = angle depuis l'origine du point and module de Z = elevation from earth center
+    # Generate complex coordinates of each point (relief). We have arg(Z) = angle from point origin and modulus of Z = elevation from earth center
     Z = np.array([trigo_to_complex(elevation_from_earth_center[i], angle_from_origin[i]) for i in range(samples)])
 
     real_Z = Z.real #X coordinate
@@ -78,7 +96,7 @@ def summit_is_visible_online(location_point, location_summit, plot = False, offs
                             
     line_of_vision = np.linspace(start_point.imag, stop_point.imag, samples)
 
-    # View is possible ?
+    # View is possible?
     view_possible_list = np.array([True if line_of_vision[i] > imag_Z[i] else False for i in range(samples)])
     view_possible = np.all([view_possible_list[i] for i in range(round(samples*0.95))]) # The for * 0.95 is not to take in consideration the collision of the line of vision with the target summit itself
 
@@ -87,28 +105,23 @@ def summit_is_visible_online(location_point, location_summit, plot = False, offs
                         data=np.column_stack([lat, long, elevation_profile, elevation_from_earth_center, angle_from_origin, Z, real_Z, imag_Z, view_possible_list]))
     
     # Get locations names
-    # calling the nominatim tool
     geoLoc = Nominatim(user_agent="GetLoc")
-
-    # passing the coordinates
-    locname_point = geoLoc.reverse((location_point[0], location_point[1]))
-    locname_summit = geoLoc.reverse((location_summit[0], location_summit[1]))
-
-    locname_point =  (locname_point.address)
-    locname_summit = (locname_summit.address)
-
+    locname_point = geoLoc.reverse((location_point[0], location_point[1])).address
+    locname_summit = geoLoc.reverse((location_summit[0], location_summit[1])).address
     print("Location point: " + locname_point)
     print("Location summit: " + locname_summit)
 
     if view_possible:
-        print("Summit in Sight!")
+        print("Summit in sight!")
     else:
         print("Summit not in Sight...")
 
     if plot == True:
-        # Create a plotting when the line of vision is below the relief level
+
+        # Create a plot when the line of vision is below the relief level to show relief masking
         masked_view = np.array([np.nan if line_of_vision[i] > imag_Z[i] else line_of_vision[i] for i in range(samples)])
-        # Plot the complex numbers
+        
+        # Plot the relief data
         fig, axs = plt.subplots(3)
         
         message = ("Summit is visible!" if view_possible else "Summit is not visible ..")
@@ -125,7 +138,7 @@ def summit_is_visible_online(location_point, location_summit, plot = False, offs
         axs[2].plot(real_Z, line_of_vision, linestyle="dashdot", color="g")
         axs[2].plot(real_Z, masked_view, linestyle="solid", color="r")
         
-        # Fill in red
+        # Fill in red masked parts
         for i in range(len(imag_Z)):
             if line_of_vision[i] < imag_Z[i]:
                 axs[1].fill_between(real_Z, imag_Z, masked_view, alpha=.5, linewidth=0, fc='red')
@@ -136,7 +149,7 @@ def summit_is_visible_online(location_point, location_summit, plot = False, offs
         axs[2].set(xlabel='Geodesic distance (km)', ylabel='//')
         plt.show()
 
-    return data, view_possible
+    return view_possible, data
 
 def summit_is_visible_fast_online(location_point, location_summit, offset_view = 10, offset_summit = -100, samples=False):
     """Return if a summit is visible or not.
