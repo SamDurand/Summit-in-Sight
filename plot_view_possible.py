5960c78c49cff4022382fd8c10d5d705f05ab6db
Sam Durand <samsam8848@gmail.com>
2023-02-06 23:52:19 +0100

    Add comments and demo functions

--- plot_view_possible.py
+++ plot_view_possible.py
@@ -1,5 +1,7 @@
 from summit_lib import plot_locations_map
 import pandas as pd
 
+# Plot the results of summit visibility
 grid_locations = pd.read_csv("data/NW_Alps_res=0.5.csv")
+
 plot_locations_map(grid_locations, color="view_possible")
