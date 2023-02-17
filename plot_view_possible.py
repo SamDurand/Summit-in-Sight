f92bdc75fa3afbfc67abfc4cc361e50b20ba49dd
SamDurand <samsam8848@gmail.com>
2023-02-17 15:21:58 +0100

    Delete data

--- plot_view_possible.py
+++ plot_view_possible.py
@@ -2,12 +2,14 @@ import pandas as pd
 from plotly.express import scatter_mapbox
 
 # Import a file
-grid_locations = pd.read_csv("data/results/Summits_Alps.csv")
+grid_locations = pd.read_csv("data/results/Grid_W_Europe_res=2.5km.csv")
+
 
 # Keep only locations when view_possible = True
 grid_locations = grid_locations[grid_locations['view_possible'] == True]
 
 # PLot locations
-fig = scatter_mapbox(grid_locations, lat="latitude", lon="longitude", mapbox_style="stamen-terrain", color_discrete_sequence=["mediumspringgreen"])
+fig = scatter_mapbox(grid_locations, lat="latitude", lon="longitude", zoom=5, mapbox_style="stamen-terrain", color_discrete_sequence=["mediumspringgreen"])
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.show()
+# fig.write_html("data/results/Grid_NW_Alps_res=0.5.html")
