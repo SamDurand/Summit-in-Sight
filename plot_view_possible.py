5eebb5fbe4319b6a75204d1986d97170b3ed842e
SamDurand <samsam8848@gmail.com>
2023-02-28 00:07:10 +0100

    Save

--- plot_view_possible.py
+++ plot_view_possible.py
@@ -2,16 +2,20 @@ import pandas as pd
 from plotly.express import scatter_mapbox
 
 # Import a file
-grid_locations = pd.read_csv("data/results/summits_results.csv")
-
+grid_locations = pd.read_csv("data/results/global_results.csv")
 
 # Keep only locations when view_possible = True
 grid_locations = grid_locations[grid_locations['view_possible'] == True]
 
 # PLot locations
-fig = scatter_mapbox(grid_locations, lat="latitude", lon="longitude", zoom=5, mapbox_style="stamen-terrain", color_discrete_sequence=["mediumspringgreen"], text="elevation", hover_name="name")
+fig = scatter_mapbox(grid_locations, lat="latitude", lon="longitude", zoom=5, mapbox_style="stamen-terrain",
+                     color_discrete_sequence=["mediumspringgreen"])
+
+# Plot summits
+# grid_locations = grid_locations[grid_locations['elevation'].notnull()]
+# fig = scatter_mapbox(grid_locations, lat="latitude", lon="longitude", zoom=5, mapbox_style="stamen-terrain", 
+#                      hover_name="name", text="elevation", color_discrete_sequence=["mediumspringgreen"])
 
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.show()
-
-# fig.write_html("data/results/summits_results.html")
+# fig.write_html("data/results/Grid_NW_Alps_res=0.5.html")
