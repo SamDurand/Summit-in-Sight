8b1aee4b5699134d6139fb515633b45e6eca34d8
Sam Durand <samsam8848@gmail.com>
2023-03-01 23:54:59 +0100

    Add new data

--- plot_view_possible.py
+++ plot_view_possible.py
@@ -10,7 +10,7 @@ For locations
 # Keep only locations when view_possible = True
 # grid_locations = grid_locations[grid_locations['view_possible'] == True]
 # fig = scatter_mapbox(grid_locations, lat="latitude", lon="longitude", zoom=5, mapbox_style="stamen-terrain",
-#                      color_discrete_sequence=["mediumspringgreen"])
+#                       color_discrete_sequence=["mediumspringgreen"])
 
 """
 For summits
@@ -19,7 +19,7 @@ For summits
 grid_locations = grid_locations.sort_values(by="view_possible", ascending=True)
 
 fig = scatter_mapbox(grid_locations, lat="latitude", lon="longitude", zoom=5, mapbox_style="stamen-terrain", 
-                     hover_name="name", text="elevation", color="view_possible" , color_discrete_sequence=["red", "mediumspringgreen"])
+                    hover_name="name", text="elevation", color="view_possible" , color_discrete_sequence=["red", "mediumspringgreen"])
 
 
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
