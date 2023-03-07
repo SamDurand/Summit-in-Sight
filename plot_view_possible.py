c14c413f924e6e62fa5b14a0b459393a47d7e147
SamDurand <samsam8848@gmail.com>
2023-03-07 23:03:45 +0100

    Add new data and map

--- plot_view_possible.py
+++ plot_view_possible.py
@@ -2,26 +2,26 @@ import pandas as pd
 from plotly.express import scatter_mapbox
 
 # Import a file
-grid_locations = pd.read_csv("data/results/summits_results.csv")
+grid_locations = pd.read_csv("data/results/global_results.csv")
 
 """
 For locations
 """
 # Keep only locations when view_possible = True
-# grid_locations = grid_locations[grid_locations['view_possible'] == True]
-# fig = scatter_mapbox(grid_locations, lat="latitude", lon="longitude", zoom=5, mapbox_style="stamen-terrain",
-#                       color_discrete_sequence=["mediumspringgreen"])
+grid_locations = grid_locations[grid_locations['view_possible'] == True]
+fig = scatter_mapbox(grid_locations, lat="latitude", lon="longitude", zoom=5, mapbox_style="stamen-terrain",
+                      color_discrete_sequence=["mediumspringgreen"])
 
 """
 For summits
 """
-# Plot summits
-grid_locations = grid_locations.sort_values(by="view_possible", ascending=True)
+# # Plot summits
+# grid_locations = grid_locations.sort_values(by="view_possible", ascending=True)
 
-fig = scatter_mapbox(grid_locations, lat="latitude", lon="longitude", zoom=5, mapbox_style="stamen-terrain", 
-                    hover_name="name", text="elevation", color="view_possible" , color_discrete_sequence=["red", "mediumspringgreen"])
+# fig = scatter_mapbox(grid_locations, lat="latitude", lon="longitude", zoom=5, mapbox_style="stamen-terrain", 
+#                     hover_name="name", text="elevation", color="view_possible" , color_discrete_sequence=["red", "mediumspringgreen"])
 
 
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.show()
-# fig.write_html("data/maps/from_summits_MontBlanc.html")
+# fig.show()
+fig.write_html("data/maps/view_possible_Mont_Blanc.html")
